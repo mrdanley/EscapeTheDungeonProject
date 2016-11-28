35e667f1e3e873df5da04e290f327bb61eab9097
Brandon <shady1765@gmail.com>
2016-11-27 22:22:28 -0800

    Make better use of java.io.Serializable properties and methods

--- src/edu/cpp/cs/cs141/final_project/SaveData.java
+++ src/edu/cpp/cs/cs141/final_project/SaveData.java
@@ -3,10 +3,6 @@ package edu.cpp.cs.cs141.final_project;
 import java.util.ArrayList;
 
 public class SaveData implements java.io.Serializable {
-
-	/**
-	 * Serial version identifier; confirms content is valid
-	 */
 	private static final long serialVersionUID = -6490327999606656237L;
 	
 	private transient Map map = null;
@@ -14,54 +10,69 @@ public class SaveData implements java.io.Serializable {
 	private transient Spy spy = null;
 	private transient ArrayList<Room> rooms = null;
 	
-	private boolean loaded = false;
-	
-	private GamePiece[][] serialGamePieces = null;
-	private Object[][] serialPowerups = null;
-	
 	public SaveData(Map m) {
-		init(m);
-	}
-	
-	/**
-	 * initializes variables, not load data yet
-	 * 
-	 * @param m
-	 */
-	private void init(Map m) {
 		map = m;
-		ninjas = new ArrayList<Ninja>();
-		rooms = new ArrayList<Room>();
-		
-		if (serialGamePieces == null) {
-			serialGamePieces = new GamePiece[map.getLength()][map.getWidth()];
-			serialPowerups = new Object[map.getLength()][map.getWidth()];
-			
-			serializeData();
-		}
+	}
+
+	/**
+	 * Attempts to return the map 
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public Map getMap() throws Exception {
+		return map;
 	}
 	
 	/**
-	 * populates serial* variables
+	 * @return
+	 * @throws Exception
 	 */
-	private void serializeData() {
+	public Ninja[] getNinjas() throws Exception {
+		return ninjas.toArray(new Ninja[ninjas.size()]);
+	}
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public Spy getSpy() throws Exception {
+		return spy;
+	}
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public Room[] getRooms() throws Exception {
+		return rooms.toArray(new Room[rooms.size()]);
+	}
+	
+	private void writeObject(java.io.ObjectOutputStream oos) throws java.io.IOException {
+		oos.defaultWriteObject();
+		
+		GamePiece[][] serialGamePieces = new GamePiece[map.getLength()][map.getWidth()];
+		Object[][] serialPowerups = new Object[map.getLength()][map.getWidth()];
+		
 		for (int i = 0; i < map.getLength(); i++) {
 			for (int j = 0; j < map.getWidth(); j++) {
 				serialGamePieces[i][j] = map.getAtLocation(i, j);
 				serialPowerups[i][j] = map.getPowerUpAtLocation(i, j);
 			}
 		}
+		oos.writeObject(serialGamePieces);
+		oos.writeObject(serialPowerups);
 	}
 	
-	/**
-	 * populates map and data variables by iterating through each "tile" 
-	 * 
-	 * @throws Exception usually a casting Exception
-	 */
-	private void loadData() throws Exception {
-		map = new Map();
-		init(map);
+	private void readObject(java.io.ObjectInputStream ois) throws ClassNotFoundException, java.io.IOException {
+		ois.defaultReadObject();
 		
+		GamePiece[][] serialGamePieces = (GamePiece[][]) ois.readObject();
+		Object[][] serialPowerups = (Object[][]) ois.readObject();
+		
+		map = new Map();
+		ninjas = new ArrayList<Ninja>();
+		rooms = new ArrayList<Room>();
 		GamePiece cursorPiece = null;
 		PowerUp cursorPowerup = null;
 		
@@ -88,44 +99,5 @@ public class SaveData implements java.io.Serializable {
 				}
 			}
 		}
-		loaded = true;
-	}
-
-	/**
-	 * Attempts to return the map 
-	 * 
-	 * @return
-	 * @throws Exception
-	 */
-	public Map getMap() throws Exception {
-		if (!loaded) loadData();
-		return map;
-	}
-	
-	/**
-	 * @return
-	 * @throws Exception
-	 */
-	public Ninja[] getNinjas() throws Exception {
-		if (!loaded) loadData();
-		return ninjas.toArray(new Ninja[ninjas.size()]);
-	}
-
-	/**
-	 * @return
-	 * @throws Exception
-	 */
-	public Spy getSpy() throws Exception {
-		if (!loaded) loadData();
-		return spy;
-	}
-
-	/**
-	 * @return
-	 * @throws Exception
-	 */
-	public Room[] getRooms() throws Exception {
-		if (!loaded) loadData();
-		return rooms.toArray(new Room[rooms.size()]);
 	}
 }
